cb2ee58a59cdab559500f9ec971214e9f6ba65a9
isobarbaric <shahkrish2016@gmail.com>
2022-02-26 00:38:05 -0500

    GUI support added

---
+++ main.py
@@ -0,0 +1,40 @@
+
+from tkinter import *
+from reaction import *
+
+root = Tk() 
+root.title('Balancing Chemical Equations')
+
+input_field_reactants = Entry(root)
+input_field_reactants.pack() 
+
+input_field_products = Entry(root)
+input_field_products.pack() 
+
+response = Label(root, text='Waiting for input...press submit once ready')
+response.pack()
+
+def when_clicked():
+    try:
+        a = input_field_reactants.get().replace(' ', '')
+        b = input_field_products.get().replace(' ', '')
+        response.config(text = "Input processed successfully...please standby")
+        root.destroy()
+    except ValueError:
+        num_attempts += 1
+        response.config(text = f"Invalid input, please correct input format and press submit again. Number of invalid attempts: {num_attempts}")  
+
+submit_button = Button(root, text="Submit coefficients", command=when_clicked)
+submit_button.pack()
+
+root.mainloop()
+
+current_reaction = Answer(a, b)
+
+root = Tk()
+root.title('Cardano\'s Method')
+
+display_balanced_eqn = Label(root, text=f"The balanced chemical equation is {current_reaction.answerString}")
+display_balanced_eqn.pack()
+
+root.mainloop()
